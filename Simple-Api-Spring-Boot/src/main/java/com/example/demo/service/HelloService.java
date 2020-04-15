4bd294b12e3d232c14cedd9fdd4b207c2302da86
unknown <hendrasimz92@gmail.com>
2020-04-16 00:26:04 +0700

    simpl api spring boot

---
+++ Simple-Api-Spring-Boot/src/main/java/com/example/demo/service/HelloService.java
@@ -0,0 +1,15 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Sogumontar Hendra Simangunsong on 20/02/2020.
+ */
+
+@Service
+public class HelloService {
+
+    public String hello(){
+        return "hello";
+    }
+}
